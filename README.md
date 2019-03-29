3f3a64d54adf3c5fd0be6c4d7252e2a40a0914aa
huntsimon23 <hunt.simon23@gmail.com>
2019-03-29 12:29:49 -0400

    Js in progress

--- README.md
+++ README.md
@@ -14,7 +14,41 @@ In this assignment, You'll create a Trivia game using JavaScript for the logic a
 
 * If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
 
+//Define Variables & Objects:
+1. var ansTally (stores # of correct answers)
+2. var timeRemain (stores)
+3. var/obj quest1 (thru 10) with 4 answer properties (1 being .correct, 3 being .incorrect1, .incorrect2, .incorrect3)
+4. var questArray (an array of objects)
+
+//Define functions
+startGame
+1. reset timeRemain and correctAns to defaul values (15 econds, 0)
+2. JQuery display timer @ 15 seconds
+3. alert Welcome Message
+4. calls nextQuest function upon alert confirm
+
+nextQuest
+1. JQuery reveal answer buttons     
+2. JQuery replace quest(X) object parameters into defined html ID's (question, ans1, ans2, ans3, ans4)
+3. reset timeRemain to 15 seconds
+4. JQuery display timeRemain in timer-text
+4. start timer @ 15 seconds
+
+correctAns 
+1. JQuery(quest(X).correct) replace question-text with GREEN "CORRECT" look, replace ansX-button color?
+2. start 7 second timer, then call nextQuest function
+3. ansTally ++
+
 * The scenario is similar for wrong answers and time-outs.
+
+wrongAns
+1. JQuery replace question-text with RED "INCORRECT" look
+
+
+
+
+
+
 
   * If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
   * If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
---
+++ assets/javascript/app.js
@@ -0,0 +1,77 @@
+var ansTally = 0;
+var quest1 = {
+    question: "What is Sharon Carter's code name?",
+    a: "Agent 12",
+    b: "Agent 13",
+    c: "Sharon 13",
+    d: "Agent 18",
+    x: $("btn-b").data('clicked'),
+    answer: "Agent 13",
+};
+ 
+var quest2 = [];
+var questArray = [quest1, quest2];
+var i = 0
+
+$(document).ready(function(){
+// $("#start-game").click(startGame());
+$("#score-text").text(ansTally);
+console.log("ready!");
+
+function startGame() {
+console.log("Game Start");
+$(".ans").text("");
+ansTally = 0;
+i = 0
+nextQuest();
+};
+
+function layout() {
+    $("#quesion-text").text(this.question);
+    $("#btn-a").text(this.a);
+    $("#btn-b").text(this.b);
+    $("#btn-c").text(this.c);
+    $("#btn-d").text(this.d);
+};
+
+function correct() {
+    $("#question-text").text(this.answer + " is correct!"); 
+    ansTally++;
+    $("#score-text").text(ansTally);
+    setTimeout(nextQuest(), 3000);
+};
+
+function incorrect() {
+    $("#question-text").text("Incorrect. It is " + this.answer);
+    setTimeout(nextQuest(), 3000);
+};
+
+function timeup() {
+    $("#question-text").text("Slowpoke. It is " + this.answer);
+    setTimeout(nextQuest(), 3000);
+};
+
+function eval() {
+    if (this.x) {
+        correct(this);
+    } else if ($(".ans").data('clicked')) {
+        incorrect(this);
+    } else {
+        timeup(this);};
+};
+
+function nextQuest() {
+        $(".ans").text("");
+        layout(questArray[i]);
+        setTimeout(eval(questArray[i]), 7000);
+        i++
+        if (i === questArray.length) {
+            endGame();
+        }
+    };
+
+function endGame() {
+    $(".ans").text("");
+    $("#question-text").text("Oh snap.  You got " + ansTally + " correct, which means you got " + (questArray.length - ansTally) + " wrong.  As the Strange Doctor says, we are in the endGame now.  Press the button below to play again.  It's like the time-loop when he defeated Dormamu...");
+}
+});
